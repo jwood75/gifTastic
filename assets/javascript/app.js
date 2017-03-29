d640e3f1ee758afe6a1b6eaa875418c062166c17
joshua wood <joshua wood>
2017-03-29 02:02:41 -0400

    updated api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ var buttonArray = ["Judy Garland", "Clark Gable", "James Dean", "Marlon Brando",
 
     var gifName = $(this).attr("data-name");
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     $.ajax({
 
